d5ae2230de787e4887f80c59a89b6e369881e8dd
Richard Huang <flyerhzm@gmail.com>
2012-05-16 18:32:54 +0800

    manual start and shutdown new_relics agent

--- lib/newrelic-workling/instrument.rb
+++ lib/newrelic-workling/instrument.rb
@@ -14,6 +14,14 @@ DependencyDetection.defer do
   executes do
     ::Workling::Base.class_eval do
       include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
+
+      alias_method :origin_dispatch_to_worker_method, :dispatch_to_worker_method
+      def dispatch_to_worker_method(method, options)
+        NewRelic::Agent.manual_start
+        origin_dispatch_to_worker_method(method, options)
+      ensure
+        NewRelic::Agent.shutdown
+      end
     end
 
     ::Workling::Discovery.discovered.each do |clazz|
